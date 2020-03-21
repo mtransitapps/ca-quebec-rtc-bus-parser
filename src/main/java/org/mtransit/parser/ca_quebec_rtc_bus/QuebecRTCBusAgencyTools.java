a4d592b06d18943d03826371bd8b9a97bb388db4
Mathieu Méa <mathieu.mea@gmail.com>
2020-03-21 19:12:18 -0400

    Cleanup trips...

--- src/main/java/org/mtransit/parser/ca_quebec_rtc_bus/QuebecRTCBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_quebec_rtc_bus/QuebecRTCBusAgencyTools.java
@@ -1,14 +1,10 @@
 package org.mtransit.parser.ca_quebec_rtc_bus;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.TreeMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.MTLog;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
@@ -16,16 +12,14 @@ import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
-import org.mtransit.parser.mt.MGenerator;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MFrequency;
 import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MSchedule;
-import org.mtransit.parser.mt.data.MServiceDate;
-import org.mtransit.parser.mt.data.MSpec;
-import org.mtransit.parser.mt.data.MStop;
 import org.mtransit.parser.mt.data.MTrip;
-import org.mtransit.parser.mt.data.MTripStop;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // https://www.rtcquebec.ca/donnees-ouvertes
 // https://cdn.rtcquebec.ca/Site_Internet/DonneesOuvertes/googletransit.zip
@@ -45,11 +39,11 @@ public class QuebecRTCBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating RTC bus data...");
+		MTLog.log("Generating RTC bus data...");
 		long start = System.currentTimeMillis();
-		this.serviceIds = extractUsefulServiceIds(args, this);
+		this.serviceIds = extractUsefulServiceIds(args, this, true);
 		super.start(args);
-		System.out.printf("\nGenerating RTC bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+		MTLog.log("Generating RTC bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
 
 	@Override
@@ -97,22 +91,27 @@ public class QuebecRTCBusAgencyTools extends DefaultAgencyTools {
 		if (matcher.find()) {
 			long digits = Long.parseLong(matcher.group());
 			if (gRoute.getRouteShortName().endsWith("a")) {
-				return 10000l + digits;
+				return 10_000L + digits;
 			} else if (gRoute.getRouteShortName().endsWith("b")) {
-				return 20000l + digits;
+				return 20_000L + digits;
 			} else if (gRoute.getRouteShortName().endsWith("g")) {
-				return 70000l + digits;
+				return 70_000L + digits;
 			} else if (gRoute.getRouteShortName().endsWith("h")) {
-				return 80000l + digits;
+				return 80_000L + digits;
 			}
 		}
-		System.out.printf("\nUnexpected route ID for %s!\n", gRoute);
-		System.exit(-1);
-		return -1l;
+		MTLog.logFatal("Unexpected route ID for %s!", gRoute);
+		return -1L;
 	}
 
-	public static final Pattern NULL = Pattern.compile("([\\- ]*null[ \\-]*)", Pattern.CASE_INSENSITIVE);
-	public static final String NULL_REPLACEMENT = "";
+	@Override
+	public String getRouteShortName(GRoute gRoute) {
+		String routeShortName = gRoute.getRouteShortName();
+		return routeShortName.toUpperCase(Locale.FRENCH); // USED BY REAL-TIME API
+	}
+
+	private static final Pattern NULL = Pattern.compile("([\\- ]*null[ \\-]*)", Pattern.CASE_INSENSITIVE);
+	private static final String NULL_REPLACEMENT = "";
 
 	@Override
 	public String getRouteLongName(GRoute gRoute) {
@@ -134,36 +133,70 @@ public class QuebecRTCBusAgencyTools extends DefaultAgencyTools {
 		return AGENCY_COLOR;
 	}
 
-	private static final String COLOR_008AC9 = "008AC9";
-
-	private static final String RSN_21 = "21";
-
 	@Override
 	public String getRouteColor(GRoute gRoute) {
-		if (RSN_21.equals(gRoute.getRouteShortName())) {
-			return COLOR_008AC9;
-		}
 		return super.getRouteColor(gRoute);
 	}
 
 	@Override
-	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		mTrip.setHeadsignString(cleanTripHeadsign(gTrip.getTripHeadsign()), gTrip.getDirectionId());
+	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) { // DIRECTION ID USED BY REAL-TIME API
+		int directionId;
+		String tripHeadsign;
+		if (gTrip.getTripHeadsign().endsWith(" (Nord)")) {
+			tripHeadsign = "N-" + gTrip.getTripHeadsign().substring(0, gTrip.getTripHeadsign().length() - 7);
+			directionId = 0;
+		} else if (gTrip.getTripHeadsign().endsWith(" (Sud)")) {
+			tripHeadsign = "S-" + gTrip.getTripHeadsign().substring(0, gTrip.getTripHeadsign().length() - 6);
+			directionId = 1;
+		} else if (gTrip.getTripHeadsign().endsWith(" (Est)")) {
+			tripHeadsign = "E-" + gTrip.getTripHeadsign().substring(0, gTrip.getTripHeadsign().length() - 6);
+			directionId = 2;
+		} else if (gTrip.getTripHeadsign().endsWith(" (Ouest)")) {
+			tripHeadsign = "O-" + gTrip.getTripHeadsign().substring(0, gTrip.getTripHeadsign().length() - 8);
+			directionId = 3;
+		} else {
+			MTLog.logFatal("Unexpected trip head-sign '%s'!", gTrip);
+			return;
+		}
+		mTrip.setHeadsignString(
+				cleanTripHeadsign(tripHeadsign),
+				directionId
+		);
 	}
 
 	@Override
 	public boolean mergeHeadsign(MTrip mTrip, MTrip mTripToMerge) {
-		System.out.printf("\nUnexpected trips to merge %s & %s!\n", mTrip, mTripToMerge);
-		System.exit(-1);
+		MTLog.logFatal("Unexpected trips to merge %s & %s!", mTrip, mTripToMerge);
 		return false;
 	}
 
-	private static final Pattern REVERSE_DIRECTION = Pattern.compile("(([^\\(]*)\\s+\\((Est|Ouest|Nord|Sud)\\)$)", Pattern.CASE_INSENSITIVE);
-	private static final String REVERSE_DIRECTION_REPLACEMENT = "$3 ($2)";
+	private static final Pattern ANCIENNE_ = CleanUtils.cleanWords("l'ancienne", "ancienne");
+	private static final String ANCIENNE_REPLACEMENT = CleanUtils.cleanWordsReplacement("Anc");
+	private static final Pattern CEGER_ = CleanUtils.cleanWords("cégep", "cegep");
+	private static final String CEGERP_REPLACEMENT = CleanUtils.cleanWordsReplacement("Cgp");
+	private static final Pattern CENTRE_ = CleanUtils.cleanWords("centre", "center");
+	private static final String CENTRE_REPLACEMENT = CleanUtils.cleanWordsReplacement("Ctr");
+	private static final Pattern ECOLE_SECONDAIRE_ = CleanUtils.cleanWords("École Secondaire", "École Sec");
+	private static final String ECOLE_SECONDAIRE_REPLACEMENT = CleanUtils.cleanWordsReplacement("ES");
+	private static final Pattern PLACE_ = CleanUtils.cleanWords("place");
+	private static final String PLACE_REPLACEMENT = CleanUtils.cleanWordsReplacement("Pl");
+	private static final Pattern POINTE_ = CleanUtils.cleanWords("pointe");
+	private static final String POINTE_REPLACEMENT = CleanUtils.cleanWordsReplacement("Pte");
+	private static final Pattern TERMINUS_ = CleanUtils.cleanWords("terminus");
+	private static final String TERMINUS_REPLACEMENT = CleanUtils.cleanWordsReplacement("Term");
+	private static final Pattern UNIVERSITE_LAVAL_ = CleanUtils.cleanWords("U Laval", "U. Laval", "Univ.Laval", "Univ. Laval", "Université Laval");
+	private static final String UNIVERSITE_LAVAL_REPLACEMENT = CleanUtils.cleanWordsReplacement("U Laval");
 
 	@Override
-	public String cleanTripHeadsign(String tripHeadsign) {
-		tripHeadsign = REVERSE_DIRECTION.matcher(tripHeadsign).replaceAll(REVERSE_DIRECTION_REPLACEMENT);
+	public String cleanTripHeadsign(String tripHeadsign) { // KEEP IN SYNC WITH REAL-TIME PROVIDER
+		tripHeadsign = ANCIENNE_.matcher(tripHeadsign).replaceAll(ANCIENNE_REPLACEMENT);
+		tripHeadsign = CEGER_.matcher(tripHeadsign).replaceAll(CEGERP_REPLACEMENT);
+		tripHeadsign = CENTRE_.matcher(tripHeadsign).replaceAll(CENTRE_REPLACEMENT);
+		tripHeadsign = ECOLE_SECONDAIRE_.matcher(tripHeadsign).replaceAll(ECOLE_SECONDAIRE_REPLACEMENT);
+		tripHeadsign = PLACE_.matcher(tripHeadsign).replaceAll(PLACE_REPLACEMENT);
+		tripHeadsign = POINTE_.matcher(tripHeadsign).replaceAll(POINTE_REPLACEMENT);
+		tripHeadsign = TERMINUS_.matcher(tripHeadsign).replaceAll(TERMINUS_REPLACEMENT);
+		tripHeadsign = UNIVERSITE_LAVAL_.matcher(tripHeadsign).replaceAll(UNIVERSITE_LAVAL_REPLACEMENT);
 		tripHeadsign = CleanUtils.removePoints(tripHeadsign);
 		tripHeadsign = CleanUtils.cleanStreetTypesFRCA(tripHeadsign);
 		return CleanUtils.cleanLabelFR(tripHeadsign);
@@ -175,6 +208,7 @@ public class QuebecRTCBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabelFR(gStopName);
 	}
 
+	@NotNull
 	@Override
 	public String getStopCode(GStop gStop) {
 		return gStop.getStopId(); // using stop ID as stop code
